8ed7bd85d04b768adf792d5ff10fe3415251fde1
Tran Tan Dat <trantandat10122003@gmail.com>
2024-09-20 17:00:08 +0700

    Create for2.java

---
+++ for2.java
@@ -0,0 +1,9 @@
+package CT300_EX3_EX1;
+
+public class for2 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
